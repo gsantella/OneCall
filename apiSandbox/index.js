df883888f93f6c2176e9540c1c4d144afe8d79d7
Guido Santella <gsantella@southhills.edu>
2017-11-04 17:30:30 -0400

    added some starter routes

--- apiSandbox/index.js
+++ apiSandbox/index.js
@@ -6,9 +6,33 @@ module.exports = app;
 // Create the express router object
 var oneCallRouter = express.Router();
 
+// OneCalls
 // A GET to the root of a resource returns a list of that resource
 oneCallRouter.get('/send', function(req, res) {
-  return res.json({ msg: ['OneCall Sent'] });
+  return res.json({ msg: ['OneCall Send List'] });
+});
+
+// A GET to the root of a resource returns that resource
+oneCallRouter.get('/send/:id', function(req, res) {
+  return res.json({ msg: ['OneCall Sent' + req.params.id] });
+});
+
+// Numbers
+oneCallRouter.post('/number/:num', function(req, res) {
+  return res.json({ msg: ['OneCall Sent' + req.params.id] });
+});
+
+oneCallRouter.delete('/number/:num', function(req, res) {
+  return res.json({ msg: ['OneCall Sent' + req.params.id] });
+});
+
+// Messages
+oneCallRouter.get('/message', function(req, res) {
+  return res.json({ msg: ['OneCall Message List'] });
+});
+
+oneCallRouter.get('/message/:id', function(req, res) {
+  return res.json({ msg: ['OneCall Message' + req.params.id] });
 });
 
 app.use('/onecall', oneCallRouter);
