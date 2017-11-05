450b070704c0abc8ff09fe54e59c51c6111d1167
Guido Santella <gsantella@southhills.edu>
2017-11-05 02:45:58 -0500

    test read line

--- apiSandbox/index.js
+++ apiSandbox/index.js
@@ -38,14 +38,15 @@ oneCallRouter.get('/number', function(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
 
-  var output = { numbers: [
-    { "id": "4", "title": "1111111111" },
-    { "id": "5", "title": "222222222222" },
-    { "id": "6", "title": "33333333333" }
-  ] };
+  var output = { numbers: [] };
+    //{ "id": "4", "title": "1111111111" },
+    //{ "id": "5", "title": "222222222222" },
+    //{ "id": "6", "title": "33333333333" }
+  //] };
 
+  var counter = 1;
   lineReader.eachLine('/app/OneCall/config/numbers.txt', function(line, last) {
-    output.numbers.push({ "id": "1", "title": "1111111111" })
+    output.numbers.push({ "id": counter++, "title": line })
   });
 
   return res.json(output);
