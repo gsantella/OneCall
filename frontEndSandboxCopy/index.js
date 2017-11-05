2ee8440e051228cd51d6766f6162b4950c0eb5da
dgressick <32648718+dgressick@users.noreply.github.com>
2017-11-05 00:11:47 -0700

    modified make call

--- frontEndSandboxCopy/index.js
+++ frontEndSandboxCopy/index.js
@@ -47,11 +47,8 @@ var app = new Vue({
       this.newTodoText = ''
     },
     makeCall: function() {
-      axios.get('http://172.17.24.52/onecall/send', {
-        params: {
-          id: 1
-        }
-      })
+      alert('one call sent');
+      axios.get('http://172.17.24.52/onecall/1')
     .then(function (response) {
       // say one call sent
     })
@@ -68,6 +65,7 @@ var app = new Vue({
       axios.get('http://172.17.24.52/onecall/number')
       .then(function (response) {
       app.$data.numbers[0] = response;
+      alert('loaded numbers');
       })
       .catch(function (error) {
       alert('axios error');
