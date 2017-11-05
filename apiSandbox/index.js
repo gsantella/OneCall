99d832f6d8d047edc8b3972b7602cd988c27d1f8
Guido Santella <gsantella@southhills.edu>
2017-11-05 02:11:46 -0500

    CORS help

--- apiSandbox/index.js
+++ apiSandbox/index.js
@@ -15,12 +15,14 @@ var oneCallRouter = express.Router();
 // A GET to the root of a resource returns a list of that resource
 oneCallRouter.get('/send', function(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
   return res.json({ msg: ['OneCall Send List'] });
 });
 
 // A GET to the root of a resource returns that resource
 oneCallRouter.get('/send/:id', function(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
   const child = childProcess.execFile('/app/OneCall/scriptsSandbox/call.sh', [''], (error, stdout, stderr) => {
     if (error) {
       throw error;
@@ -34,6 +36,7 @@ oneCallRouter.get('/send/:id', function(req, res) {
 // Numbers
 oneCallRouter.get('/number', function(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
 
   var output = { numbers: [
     { "id": "1", "title": "1111111111" },
@@ -50,6 +53,8 @@ oneCallRouter.get('/number', function(req, res) {
 
 oneCallRouter.post('/number', function(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+
   var num = req.body.num;
 
   const child = childProcess.execFile('/app/OneCall/scriptsSandbox/call-add-num.sh', [num], (error, stdout, stderr) => {
@@ -64,6 +69,8 @@ oneCallRouter.post('/number', function(req, res) {
 
 oneCallRouter.delete('/number', function(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+
   var num = req.body.num;
 
   const child = childProcess.execFile('/app/OneCall/scriptsSandbox/call-del-num.sh', [num], (error, stdout, stderr) => {
@@ -79,11 +86,15 @@ oneCallRouter.delete('/number', function(req, res) {
 // Messages
 oneCallRouter.get('/message', function(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+
   return res.json({ msg: ['OneCall Message List'] });
 });
 
 oneCallRouter.get('/message/:id', function(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+  
   return res.json({ msg: ['OneCall Message' + req.params.id] });
 });
 
