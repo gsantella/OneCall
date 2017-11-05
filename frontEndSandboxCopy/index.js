ad735441b268d3b949f5009d698539974a580bca
dgressick <32648718+dgressick@users.noreply.github.com>
2017-11-05 01:33:27 -0700

    KJLKLRG

--- frontEndSandboxCopy/index.js
+++ frontEndSandboxCopy/index.js
@@ -11,20 +11,7 @@ var app = new Vue({
   el: '#todo-list-example',
   data: {
     newTodoText: '',
-    numbers: [
-      {
-        id: 1,
-        title: 'Do the dishes',
-      },
-      {
-        id: 2,
-        title: 'Take out the trash',
-      },
-      {
-        id: 3,
-        title: 'Mow the lawn'
-      }
-    ],
+    numbers: "",
     nextTodoId: 4
   },
   methods: {
